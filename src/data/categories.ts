b8b7b3bc4b1d974757ca6bc5cf7f42dfe444d2ef
MobinaLalani <MobinaLalani529@gmail.com>
2026-07-12 09:49:14 +0330

    change categories type and image

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,40 +1,47 @@
 import { Category } from "@/src/types/category";
 
+import mobileImage from "@/public/Image/categories/mobile.jpg";
+import laptopImage from "@/public/Image/categories/laptop.png";
+import headphoneImage from "@/public/Image/categories/headphone.jpg";
+// import watchImage from "@/public/Image/categories/watch.png";
+// import cameraImage from "@/public/Image/categories/camera.png";
+// import gamingImage from "@/public/Image/categories/gaming.png";
+
 export const categories: Category[] = [
   {
     id: 1,
     title: "موبایل",
     slug: "mobile",
-    image: "/images/categories/mobile.png",
+    image: mobileImage,
   },
   {
     id: 2,
     title: "لپ تاپ",
     slug: "laptop",
-    image: "/images/categories/laptop.png",
+    image: laptopImage,
   },
   {
     id: 3,
     title: "هدفون",
     slug: "headphone",
-    image: "/images/categories/headphone.png",
+    image: headphoneImage,
   },
   {
     id: 4,
     title: "ساعت",
     slug: "watch",
-    image: "/images/categories/watch.png",
+    image: laptopImage,
   },
   {
     id: 5,
     title: "دوربین",
     slug: "camera",
-    image: "/images/categories/camera.png",
+    image: laptopImage,
   },
   {
     id: 6,
     title: "کنسول",
     slug: "gaming",
-    image: "/images/categories/xbox.png",
+    image: laptopImage,
   },
 ];
--- src/types/category.ts
+++ src/types/category.ts
@@ -1,6 +1,8 @@
+import { StaticImageData } from "next/image";
+
 export interface Category {
   id: number;
   title: string;
   slug: string;
-  image: string;
+  image: StaticImageData;
 }
