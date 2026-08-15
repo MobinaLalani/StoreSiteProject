dc31fc2025f8610d1c76d1e2ae894658050fe594
mobina lalani <mobinalalani529@gmail.com>
2026-08-15 11:58:39 +0330

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,10 @@ import ReactQueryProvider from "@/src/providers/ReactQueryProvider";
 
 export const metadata: Metadata = {
   manifest: "/manifest.webmanifest",
-
   formatDetection: {
     email: false,
     address: false,
     telephone: false,
-  },
-
-  verification: {
-    google: "mR-wwsjNPHLqyfRj4xjqcLPpzA4yaCEQtGcjlURdOuw",
   },
 };
 
@@ -30,6 +25,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="mR-wwsjNPHLqyfRj4xjqcLPpzA4yaCEQtGcjlURdOuw"
+        />
+      </head>
+
       <body suppressHydrationWarning className="antialiased">
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
